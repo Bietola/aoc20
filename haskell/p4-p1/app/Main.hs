2c38c16eed5d8918577f8edc82e69609d726628f
Bietola <dincio.montesi@gmail.com>
2020-12-09 16:21:06 +0100

    Solve 4

--- haskell/p4-p1/app/Main.hs
+++ haskell/p4-p1/app/Main.hs
@@ -1,34 +1,43 @@
 module Main where
 
-preciseQuot lhs rhs =
-  if lhs `mod` rhs /= 0
-    then undefined
-    else lhs `quot` rhs
+{-# LANGUAGE OverloadedStrings #-}
 
-bin2id :: String -> (Int, Int)
-bin2id str = frontBack2id $ takeWhile isFrontBack str
+import Data.List.Split
+import Data.List
+import Data.Maybe
+import Text.Printf
+import Data.Function
 
-frontBot2id :: String -> Int
-frontBot2id str =
-  case binPart 'F' 'B' 0 128 str of
-    (x, y) | x == y -> x
-    _ -> undefined
+solve2 :: String -> Int
+solve2 input =
+  let orderedIds = (sort . map seat2id . lines) input
+    in zip [(orderedIds !! 0)..] orderedIds
+       & find (\(idx, id) -> idx /= id)
+       & fmap (\(idx, _) -> idx)
+       & fromJust
 
-leftRight2id :: String -> (Int, Int)
-leftRight2id str =
-  case binPart 'L' 'R' 0 undefined str of
-    (x, y) | x == y -> x
-    _ -> undefined
+solve1 :: String -> Int
+solve1 = maximum . map seat2id . lines
 
+seat2id :: String -> Int
+seat2id syms =
+  let div = fromJust $ findIndex (`elem` ['L', 'R']) syms -- simplify findIndex
+      (rowSyms, colSyms) = splitAt div syms
+      rowId = symbols2binary 'F' 'B' rowSyms
+      colId = symbols2binary 'L' 'R' colSyms
+    in rowId * 8 + colId
 
-binPart:: a -> a -> Int -> Int -> [a] -> (Int, Int)
-binPart lowSym highSym initMin initMax = foldr go (initMin, initMax) . reverse
-  where go frontBot (min, max) =
-          let middle = (max - min) `preciseQuot` 2
-            in case frontBot of
-              lowSym -> (min, max - middle)
-              highSym -> (min + middle, max)
-              _ -> undefined
+symbols2binary :: Char -> Char -> [Char] -> Int
+symbols2binary zero one = foldl go 0
+  where go acc sym =
+          case sym of
+            s | s == zero -> acc * 2
+            s | s == one  -> acc * 2 + 1
+            s -> error $ printf "Unhandled sym: %c" s
 
 main :: IO ()
-main = print "hello world"
+main = do
+  contents <- getContents
+  print $ solve1 contents
+  print ""
+  print $ solve2 contents
