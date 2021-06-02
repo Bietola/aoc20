d2a75e280105fead75574a146fd29647a9d5444c
Bietola <dincio.montesi@gmail.com>
2021-06-02 13:25:11 +0200

    Solve p4 with db

--- haskell/p4/app/Main.hs
+++ haskell/p4/app/Main.hs
@@ -1,10 +1,27 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Main where
 
+import Control.Arrow
 import Data.Function
+import Data.List
 import Data.List.Split
 import qualified Data.Map as M
+import Debug.Trace
+
+parse =
+  lines >>>
+  splitWhen (== "") >>>
+  map
+    (M.fromList .
+     map (intoTuple . splitWhen (== ':')) . traceShowId . words . unwords)
+  where
+    intoTuple =
+      \case
+        [x, y] -> (x, y)
+        x -> traceShow x undefined
 
 main :: IO ()
 main = do
   inp <- getContents
-  print $ inp & lines & splitWhen (=="") & fmap (fmap (splitOn ":"))
+  print $ parse inp
